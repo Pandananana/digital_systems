998e55dc7cd4e48780da5663ee57c0bd0d37eaf1
Pandananana <andreaswendelboe.dk@gmail.com>
2023-02-28 17:30:26 +0100

    Shifter designed and simulated

--- PWA/PWA.srcs/sim_1/new/TB_Shifter.vhd
+++ PWA/PWA.srcs/sim_1/new/TB_Shifter.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 02/15/2023 11:02:41 AM
+-- Create Date: 02/28/2023 05:22:55 PM
 -- Design Name: 
--- Module Name: Shifter - Behavioral
+-- Module Name: TB_Shifter - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -17,8 +17,10 @@
 -- Additional Comments:
 -- 
 ----------------------------------------------------------------------------------
-LIBRARY IEEE;
-USE IEEE.STD_LOGIC_1164.ALL;
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -29,19 +31,36 @@ USE IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-ENTITY Shifter IS
+entity TB_Shifter is
+--  Port ( );
+end TB_Shifter;
+
+architecture Behavioral of TB_Shifter is
+    component Shifter IS
     PORT (
         B : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
         H_Select : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
         h : OUT STD_LOGIC_VECTOR (7 DOWNTO 0));
-END Shifter;
+END component;
+    SIGNAL B :  STD_LOGIC_VECTOR (7 DOWNTO 0);
+    SIGNAL H_Select :  STD_LOGIC_VECTOR (1 DOWNTO 0);
+    SIGNAL h :  STD_LOGIC_VECTOR (7 DOWNTO 0);
 
-ARCHITECTURE Behavioral OF Shifter IS
+begin
+    UUT: Shifter port map(
+       B,H_Select,h 
+    );
+    B <= "11111111";
 
-BEGIN
-    h <=    B                       WHEN H_Select = "00" ELSE
-            '0' & B(7 DOWNTO 1)     WHEN H_Select = "01" ELSE
-            B(6 DOWNTO 0) & '0'     WHEN H_Select = "10" ELSE
-            B;
-
-END Behavioral;
+    stim_proc : process
+    begin
+        H_Select <="00";
+        wait for 10 ns;
+        H_Select <="01";
+        wait for 10 ns;
+        H_Select <="10";
+        wait for 10 ns;
+        H_Select <="11";
+        wait;
+    end process;
+end Behavioral;
