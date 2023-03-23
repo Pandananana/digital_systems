7fcad156286752cddc03e10e3d12bf759430fc74
Pandananana <andreaswendelboe.dk@gmail.com>
2023-03-23 15:10:50 +0100

    InstructionDecoder Screenshots done

--- PWB/PWB.sim/sim_1/behav/xsim/xsim.dir/TB_InstructionDecoderController_behav/webtalk/xsim_webtalk.tcl
+++ PWB/PWB.sim/sim_1/behav/xsim/xsim.dir/TB_InstructionDecoderController_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir /zhome/66/2/168935/uni/digital_systems/PWB/PWB.sim/sim_1/behav/xsim/xsim.dir/TB_InstructionDecoderController_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Thu Mar 23 13:43:33 2023" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Thu Mar 23 14:28:32 2023" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2258646" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "LIN64" -context "software_version_and_target_device"
@@ -14,11 +14,11 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "1f736ff0-b88b-45d9-845c-3400afb2451e" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "55271ff80f134bc98abff9654ec21120" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "42" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "43" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Scientific" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "Scientific Linux release 7.9 (Nitrogen)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Xeon(R) CPU E5-2660 v3 @ 2.60GHz" -context "user_environment"
-webtalk_add_data -client project -key cpu_speed -value "2899.926 MHz" -context "user_environment"
+webtalk_add_data -client project -key cpu_speed -value "2599.841 MHz" -context "user_environment"
 webtalk_add_data -client project -key total_processors -value "2" -context "user_environment"
 webtalk_add_data -client project -key system_ram -value "134.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "660 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.01_sec" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Memory -value "126176_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1093988535 -regid "176339219_1777488102_210753852_502" -xml /zhome/66/2/168935/uni/digital_systems/PWB/PWB.sim/sim_1/behav/xsim/xsim.dir/TB_InstructionDecoderController_behav/webtalk/usage_statistics_ext_xsim.xml -html /zhome/66/2/168935/uni/digital_systems/PWB/PWB.sim/sim_1/behav/xsim/xsim.dir/TB_InstructionDecoderController_behav/webtalk/usage_statistics_ext_xsim.html -wdm /zhome/66/2/168935/uni/digital_systems/PWB/PWB.sim/sim_1/behav/xsim/xsim.dir/TB_InstructionDecoderController_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_transmit -clientid 2065290904 -regid "176339219_1777488102_210753852_502" -xml /zhome/66/2/168935/uni/digital_systems/PWB/PWB.sim/sim_1/behav/xsim/xsim.dir/TB_InstructionDecoderController_behav/webtalk/usage_statistics_ext_xsim.xml -html /zhome/66/2/168935/uni/digital_systems/PWB/PWB.sim/sim_1/behav/xsim/xsim.dir/TB_InstructionDecoderController_behav/webtalk/usage_statistics_ext_xsim.html -wdm /zhome/66/2/168935/uni/digital_systems/PWB/PWB.sim/sim_1/behav/xsim/xsim.dir/TB_InstructionDecoderController_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
