0cdd8d5c5726ed0e9bbe00c8e8885d3f4928b4d3
Pandananana <andreaswendelboe.dk@gmail.com>
2023-04-13 12:49:02 +0200

    Finished PortReg. Ready for sim

--- PWF/PWF.srcs/sources_1/new/PortReg8X8.vhd
+++ PWF/PWF.srcs/sources_1/new/PortReg8X8.vhd
@@ -33,7 +33,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity PortReg8X8 is
     Port ( clk : in STD_LOGIC;
-           reset : in STD_LOGIC;
            MW : in STD_LOGIC;
            Data_in : in STD_LOGIC_VECTOR (7 downto 0);
            Adress_in : in STD_LOGIC_VECTOR (7 downto 0);
@@ -65,50 +64,105 @@ ARCHITECTURE Behavioral OF PortReg8x8 IS
     
 BEGIN
 
-load <= "001" when Adress_in = "11111000" else
-        "010" when Adress_in = "11111001" else
-        "100" when Adress_in = "11111010" else
-        "000";
-      
+    ---- Register 8x8 ----
+
+    load <= "001" when Adress_in = "11111000" else
+            "010" when Adress_in = "11111001" else
+            "100" when Adress_in = "11111010" else
+            "000";
 
     MR0_gen : FOR i IN 0 TO 7 GENERATE
         UMR0_gen : component DFF_EN_RS
-        PORT MAP(Data_in(i), Reset, load(0), Clk, MR0(i));
+        PORT MAP(Data_in(i), '0', load(0), Clk, MR0(i));
     END GENERATE;
 
     MR1_gen : FOR i IN 0 TO 7 GENERATE
         UMR1_gen : component DFF_EN_RS
-        PORT MAP(Data_in(i), Reset, load(1), Clk, MR1(i));
+        PORT MAP(Data_in(i), '0', load(1), Clk, MR1(i));
     END GENERATE;
 
     MR2_gen : FOR i IN 0 TO 7 GENERATE
         UMR2_gen : component DFF_EN_RS
-        PORT MAP(Data_in(i), Reset, load(2), Clk, MR2(i));
+        PORT MAP(Data_in(i), '0', load(2), Clk, MR2(i));
     END GENERATE;
 
     MR3_gen : FOR i IN 0 TO 7 GENERATE
         UMR3_gen : component DFF_EN_RS
-        PORT MAP(SW(i), Reset, BTNR, Clk, MR3(i));
+        PORT MAP(SW(i), '0', BTNR, Clk, MR3(i));
     END GENERATE;
 
     MR4_gen : FOR i IN 0 TO 7 GENERATE
         UMR4_gen : component DFF_EN_RS
-        PORT MAP(SW(i), Reset, BTNL, Clk, MR4(i));
+        PORT MAP(SW(i), '0', BTNL, Clk, MR4(i));
     END GENERATE;
 
     MR5_gen : FOR i IN 0 TO 7 GENERATE
         UMR5_gen : component DFF_EN_RS
-        PORT MAP(SW(i), Reset, BTND, Clk, MR5(i));
+        PORT MAP(SW(i), '0', BTND, Clk, MR5(i));
     END GENERATE;
 
     MR6_gen : FOR i IN 0 TO 7 GENERATE
         UMR6_gen : component DFF_EN_RS
-        PORT MAP(SW(i), Reset, BTNU, Clk, MR6(i));
+        PORT MAP(SW(i), '0', BTNU, Clk, MR6(i));
     END GENERATE;
 
     MR7_gen : FOR i IN 0 TO 7 GENERATE
         UMR7_gen : component DFF_EN_RS
-        PORT MAP(SW(i), Reset, BTNC, Clk, MR7(i));
+        PORT MAP(SW(i), '0', BTNC, Clk, MR7(i));
     END GENERATE;
+
+
+    ---- Write and read
+    MR_Process: process (clk)
+    begin
+        if rising_edge(CLK) then
+            if MW='1' then
+                case Adress_in is
+                    when "11111000" =>
+                        MR0 <= Data_in;
+
+                    when "11111001" =>
+                        MR1 <= Data_in;
+
+                    when "11111010" =>
+                        MR2 <= Data_in;
+                
+                    when others =>
+                        null;
+                end case;
+            else
+                MMR <= '1';
+                case Adress_in is
+                    when "11111000" =>
+                        Data_outR <= "00000000" & MR0;
+
+                    when "11111001" =>
+                        Data_outR <= "00000000" & MR1;
+
+                    when "11111010" =>
+                        Data_outR <= "00000000" & MR2;
+
+                    when "11111011" =>
+                        Data_outR <= "00000000" & MR3;
+
+                    when "11111100" =>
+                        Data_outR <= "00000000" & MR4;
+
+                    when "11111101" =>
+                        Data_outR <= "00000000" & MR5;
+                    
+                    when "11111110" =>
+                        Data_outR <= "00000000" & MR6;
+
+                    when "11111111" =>
+                        Data_outR <= "00000000" & MR7;
+                
+                    when others =>
+                        MMR <= '0';
+                end case;
+            end if;
+        end if;
+    end process;
+
 
 end Behavioral;
